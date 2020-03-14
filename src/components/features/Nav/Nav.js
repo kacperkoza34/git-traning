462969351f56b0cd6bcff637dbc9d0252cb3e909
Bartek <ba>
2020-03-14 21:14:04 +0100

    try to learn git

--- src/components/features/Nav/Nav.js
+++ src/components/features/Nav/Nav.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import styles from './Nav.module.scss';
-//import {NavLink} from 'react-router-dom';
-//import { navConfig } from './NavConfig';
-
+import {NavLink} from 'react-router-dom';
+import { navConfig } from './NavConfig';
 function Nav() {
   return (
     <div className={styles.nav}>
-      <ul>
-        <li>Start</li>
-        <li>O mnie</li>
-        <li>Portfolio</li>
-        <li>Kontakt</li>
+      <ul> {
+        navConfig.map( ({title, link}) =>(
+          <li>
+            <NavLink to={link} key={link}>{title}</NavLink>
+          </li>
+      )
+      )
+      }
       </ul>
-    </div>
-  );
+    </div> );
 }
-
 export default Nav;

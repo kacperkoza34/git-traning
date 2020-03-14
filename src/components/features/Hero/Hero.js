89311d40883ff5c89275b3f378a228e2849757ce
Kacper Koza <kacperkoza34@gmail.com>
2020-03-14 14:02:32 +0100

    Add gsap to Hero

--- src/components/features/Hero/Hero.js
+++ src/components/features/Hero/Hero.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import styles from './Hero.module.scss';
+import { ReactComponent as Icon } from './Icon.svg';
+import gsap from 'gsap';
 
 function Hero() {
+  const wrapper = useRef(null);
+
+  useEffect(() => {
+    const [elements] = wrapper.current.children;
+
+    const message1 = elements.querySelectorAll('.message1');
+    const message2 = elements.querySelectorAll('.message2');
+    const message3 = elements.querySelectorAll('.message3');
+
+    console.log(message1);
+  });
+
   return (
     <div className={styles.hero}>
-
+      <div ref={wrapper} className={styles.wrapper}>
+        <Icon/>
+      </div>
     </div>
   );
 }
